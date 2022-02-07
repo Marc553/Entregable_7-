f03003bca1bdf13f407f6270845bbe15f422a989
Marc553 <marcramis2016@gmail.com>
2022-02-07 17:26:50 +0100

    .

--- Assets/Scripts/PalyerController.cs
+++ Assets/Scripts/PalyerController.cs
@@ -6,25 +6,65 @@ public class PalyerController : MonoBehaviour
 {
     public float playerSpeed = 20f; //velocidad con la que se empuja la player
     private Rigidbody playerRigidbody;  //rigidbody del player 
-    private float yRange = 14;
+    private float yRange = 0.23f; //limite del suelo donde se acaba el juego 
+   
+    
+    private bool gameOver; // se acaba todo 
+    public int recolectables = 0; //numero de recolectables recogidos
+    
+    
+    private AudioSource playerAudioSource; //ruido que se escucahara al efectuar el salto
+    public AudioClip boingClip;
+    public AudioClip blipClip;
+    public AudioClip boomClip;
+
+    public ParticleSystem particleRecolectable;
+    public ParticleSystem particleExprosion;
+ 
 
     void Start()
     {
-        playerRigidbody = GetComponent<Rigidbody>();
+        gameOver = false;
+        playerRigidbody = GetComponent<Rigidbody>(); //entra en la componente rigidbody del player 
+        playerAudioSource = GetComponent<AudioSource>();
     }
 
   
     void Update()
     {
-        if (Input.GetKey(KeyCode.Space))
+        //Ejecuta la acción de ejercer fuerza cuando se pulsa el espacio 
+        if (Input.GetKey(KeyCode.Space)) 
         {
             playerRigidbody.AddForce(Vector3.up * playerSpeed); //fuerza ejercida al player hacia arriba
+            playerAudioSource.PlayOneShot(boingClip, 1);
         }
 
-        //if (transform.position.y > yRange)
+        //Si el player pasa esta posición el juego se acaba (es el suelo)
+        if (transform.position.y < yRange) 
         {
-            
+            Time.timeScale = 0;
         }
 
     }
+
+    /*private void OntriggerEnter(Collider otherCollider)
+    {
+        //Si se choca con el objeto con la etiqueta llamada "recolectable" suma 1 al recolectable
+        if(otherCollider.gameObject.CompareTag("recolectable"))
+        {
+            recolectables++;
+            Debug.Log(recolectables);
+           playerAudioSource.PlayOneShot(boingClip, 1);
+        }
+    
+        if(otherCollider.gameObject.CompareTag("bomba"))
+        {
+           playerAudioSource.PlayerOneShot(boomClip, 1);
+           Time.timeScale = 0;
+           
+        }
+    }*/
+
+
+
 }
